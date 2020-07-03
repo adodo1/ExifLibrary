7d471f0e686a32bc347b0b91c8b577c629ea0132
Warren Galyen <wgalyen@gmail.com>
2020-07-03 07:30:18 -0700

    Add test cases

---
+++ UnitTests/ExifEnumProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using ExifLibrary;
+
+namespace UnitTests
+{
+    public class ExifEnumProperty
+    {
+        [Fact]
+        public void Orientation()
+        {
+            for (var i = 1; i <= 8; i++)
+            {
+                var img = ImageFile.FromFile(TestHelpers.TestImagePath("Orientation", "Orientation_" + i.ToString() + ".jpg"));
+                var orientation = img.Properties.Get<ExifEnumProperty<Orientation>>(ExifTag.Orientation);
+                Assert.Equal((ExifLibrary.Orientation)i, orientation);
+            }
+        }
+    }
+}
---
+++ UnitTests/TestHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace UnitTests
+{
+    public static class TestHelpers
+    {
+        private static string TestImagesPath
+        {
+            get
+            {
+                var path = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + @"..\..\..\";
+                path = Path.Combine(path, "TestImages");
+                return path;
+            }
+        }
+
+        public static string TestImagePath(string subfolder, string filename)
+        {
+            return Path.Combine(TestImagesPath, subfolder, filename);
+        }
+    }
+}
